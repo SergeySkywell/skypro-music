4854d32c32e391d22a7707b90a71cab3081a62e3
Sergey Laletin <Quercue@gmail.com>
2025-08-26 17:30:19 +0300

    refactor: replace classes in MainSidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Bar from "@/components/Bar/Bar";
 import Navigation from "@/components/Navigation/Navigation";
 import Centerblock from "@/components/Cetnerblock/Centerblock";
+import MainSidebar from "@/components/MainSidebar/MainSidebar";
 
 export default function Home() {
   return (
@@ -13,53 +14,7 @@ export default function Home() {
         <main className={"main"}>
           <Navigation />
           <Centerblock />
-          <div className={"main__sidebar"}>
-            <div className={"sidebar__personal"}>
-              <p className={"sidebar__personalName"}>Sergey.Ivanov</p>
-              <div className={"sidebar__icon"}>
-                <svg>
-                  <use xlinkHref="/img/icon/sprite.svg#logout"></use>
-                </svg>
-              </div>
-            </div>
-            <div className={"sidebar__block"}>
-              <div className={"sidebar__list"}>
-                <div className={"sidebar__item"}>
-                  <Link className={"sidebar__link"} href="#">
-                    <Image
-                      className={"sidebar__img"}
-                      src="/img/playlist01.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={170}
-                    />
-                  </Link>
-                </div>
-                <div className={"sidebar__item"}>
-                  <Link className={"sidebar__link"} href="#">
-                    <Image
-                      className={"sidebar__img"}
-                      src="/img/playlist02.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={170}
-                    />
-                  </Link>
-                </div>
-                <div className={"sidebar__item"}>
-                  <Link className={"sidebar__link"} href="#">
-                    <Image
-                      className={"sidebar__img"}
-                      src="/img/playlist03.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={170}
-                    />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          <MainSidebar />
         </main>
         <Bar />
         <footer className="footer"></footer>
